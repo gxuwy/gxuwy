111fb530b5b06237985d0dbc8c61c60d7801db41
gxuwy <982556628@qq.com>
2019-01-04 21:04:49 +0800

    登录验证功能

---
+++ js/login.js
@@ -0,0 +1,87 @@
+$(function(){
+   //使用表单校验插件
+ $("#form").bootstrapValidator({
+    //2. 指定校验时的图标显示，默认是bootstrap风格
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+  
+    //3. 指定校验字段
+    fields: {
+      //校验用户名，对应name表单的name属性
+      username: {
+        validators: {
+          //不能为空
+          notEmpty: {
+            message: '用户名不能为空'
+          },
+          //长度校验
+          stringLength: {
+            min: 2,
+            max: 6,
+            message: '用户名长度必须在2到6之间'
+          },
+          //正则校验
+          regexp: {
+            regexp: /^[a-zA-Z0-9_\.]+$/,
+            message: '用户名由数字字母下划线和.组成'
+          },
+          callback:{
+              message:"用户名不存在"
+          }
+        }
+      },
+      password:{
+        validators: {
+            //不能为空
+            notEmpty: {
+              message: '密码不能为空'
+            },
+            //长度校验
+            stringLength: {
+              min: 6,
+              max: 12,
+              message: '用户名长度必须在6到12之间'
+            },
+            callback:{
+                message:"密码错误"
+            }
+          }
+      }
+    }
+  
+  });
+
+  $("#form").on('success.form.bv', function (e) {
+    e.preventDefault();
+    $.ajax({
+        type:"post",
+        url:"/employee/employeeLogin",
+        data:$("#form").serialize(),
+        dataType:"json",
+        success:function( info ){
+            if(info.error ===1000){
+                $('#form').data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
+                return;               
+            }
+            if ( info.error === 1001 ) {
+                // alert( "密码错误" );
+                $('#form').data("bootstrapValidator").updateStatus("password", "INVALID", "callback");
+                return;
+              }
+              if ( info.success ) {
+                // 跳转到首页
+                location.href = "index.html";
+                return;
+              }
+        }
+
+    })
+});
+$('[type="reset"]').on("click",function(){
+  $('#form').data("bootstrapValidator").resetForm();//重置表单，并且会隐藏所有的错误提示和图标
+})
+  
+})
